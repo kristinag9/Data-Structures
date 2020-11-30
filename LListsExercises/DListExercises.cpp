0d4f26c8e539932e00b9e726b3c7ad940ad61da5
Kristina Tsekova <47756287+kristinag9@users.noreply.github.com>
2020-11-30 12:28:54 +0200

    Revert "Revert "Exercises""
    This reverts commit e94b17d517239738d6ce16e8de444a8eb7e6100b.

---
+++ LListsExercises/DListExercises.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <stack>
+
+struct Node {
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int _data, Node* _next = nullptr, Node* _prev = nullptr) {
+        data = _data;
+        next = _next;
+        prev = _prev;
+    }
+};
+
+// Задача 8. Да се напише функция, която проверява дали даден двусвързан списък е палиндром.
+bool isPalindrome(Node* start) {
+    if (start == nullptr) {
+        return true;
+    }
+
+    Node* current = start;
+
+    std::stack<int> elementsFromList;
+    while (current != nullptr) {
+        elementsFromList.push(current->data);
+        current = current->next;
+    }
+
+    current = start;
+
+    while (elementsFromList.empty() == false) {
+        if (current->data != elementsFromList.top()) {
+            return false;
+        }
+        current = current->next;
+        elementsFromList.pop();
+    }
+
+    return true;
+}
+
+Node* reverse(Node* start) {
+    if (start == nullptr) {
+        return nullptr;
+    }
+
+    Node* current = new Node(start->data);
+
+    start = start->next;
+    while (start != nullptr) {
+        current = new Node(start->data, current);
+        current->next->prev = current;
+        start = start->next;
+    }
+
+    return current;
+}
+
+int main() {
+    Node* n1 = new Node(3);
+    Node* n2 = new Node(2);
+    Node* n3 = new Node(1);
+
+    n1->next = n2;
+    n2->prev = n1;
+    n2->next = n3;
+    n3->prev = n2;
+
+    std::cout << isPalindrome(n1);
+
+    Node* result = reverse(n1);
+    while (result != nullptr)
+    {
+        std::cout << result->data << ' ';
+        result = result->next;
+    }
+
+    return 0;
+}
